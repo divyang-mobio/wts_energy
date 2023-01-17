a58d089ba80a246a9167ec997f70923696461430
divyang-mobio <107481877+divyang-mobio@users.noreply.github.com>
2023-01-17 10:54:01 +0530

    init

--- lib/main.dart
+++ lib/main.dart
@@ -11,9 +11,10 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-        title: 'Flutter Demo',
+        title: 'Toolbox Suite',
+        debugShowCheckedModeBanner: false,
         theme: ThemeData(
-            primarySwatch: Colors.blue,
+            primarySwatch: Colors.grey,
             appBarTheme: const AppBarTheme(
                 elevation: 0,
                 centerTitle: true,
